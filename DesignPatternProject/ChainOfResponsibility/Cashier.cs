3545d361ec788f8ac797caed8ab1bbd64e2f871d
CicekHuseyin <95969675+CicekHuseyin@users.noreply.github.com>
2024-08-03 16:11:19 +0300

    Add project files.

---
+++ DesignPatternProject/ChainOfResponsibility/Cashier.cs
@@ -0,0 +1,36 @@
+﻿using DesignPatternProject.DAL;
+using DesignPatternProject.Models;
+
+namespace DesignPatternProject.ChainOfResponsibility
+{
+    public class Cashier : Employee
+    {
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            var context = new Context();
+            if (req.Amount <= 100000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount;
+                customerProcess.CustomerName = req.CustomerName;
+                customerProcess.ProcessDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+                customerProcess.EmployeeName = req.EmployeeName;
+                customerProcess.EmployeeDescription = "Müşterinin talep ettiği tutar müşteriye ödendi, işlem kapatıldı.";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            else if (NextApprover != null)
+            {
+                var customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount;
+                customerProcess.CustomerName = req.CustomerName;
+                customerProcess.ProcessDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+                customerProcess.EmployeeName = req.EmployeeName;
+                customerProcess.EmployeeDescription = "Müşterinin talep ettiği tutar veznedar tarafından ödenemedi, işlem şube müdür yardımcısına yönlendirildi.";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+                NextApprover.ProcessRequest(req);
+            }
+        }
+    }
+}
---
+++ DesignPatternProject/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPatternProject.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-KR485FT\\SQLEXPRESS;initial catalog=DbDesignPatternProject;integrated security=true");
+        }
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
